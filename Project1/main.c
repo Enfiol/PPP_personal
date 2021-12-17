2b36243a940d0a841c95869effd9681720e3a7b0
enfiol <enfiol@mail.ru>
2021-12-17 19:25:27 +0300

    Реализован метод

--- Project1/main.c
+++ Project1/main.c
@@ -1,81 +1,157 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
 
-int** loadMatrix(FILE* inputFile, int m, int n)
+int** loadMatrix(/*FILE* inputFile,*/ int m, int n)
 {
     
-    int **matrix = (int**)malloc(sizeof(int*)*n);
+    double **matrix = (int**)malloc(sizeof(int*)*n);
     if (matrix == NULL)
     {
-        printf("Cannot allocate memory for matrix");
+        printf("Cannot allocate memory for matrix\n");
+        exit(1);
     }
     for (int i = 0; i < n; i++)
     {
-        matrix[i] = (int*)malloc(sizeof(int) * m);
-        if (matrix[i] = NULL)
+        matrix[i] = (int*)malloc(sizeof(double) * m);
+        if (matrix[i] == NULL)
         {
-            printf("Cannot allocate memory for row of matrix");
+            printf("Cannot allocate memory for row of matrix\n");
+            exit(1);
         }
     }
+    // Матрица из файла
+    /*char* s;
+    s = (char*)malloc(sizeof(char) * 100);
+    char c;
 
+    for(int i = 0; i < n; i++)
+    {
+        
+        for(int j = 0; j < m; j++)
+        {
+            for (int k = 0; k < 100 && s[k]!='\0'; k++)
+            {
+                s[k] = 0;
+            }
+            for (int k = 0; (c = fgetc(inputFile)) != EOF && c != '\n' && c != ' '; k++)
+            {
+                s[k] = c;
+            }
+            matrix[i][j] = atoi(s);
+        }
+    }
+    
+   
+    if (s)
+    {
+        free(s);
+    }*/
+
+    // Генерация матрицы
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+    
+    
+    return matrix;
+}
+
+
+void printMatrix(FILE *outputFile, double **matrix, int m, int n)
+{
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            fprintf(outputFile, "%f ", matrix[i][j]);
+        }
+        fprintf(outputFile, "\n");
+    }
+    fprintf(outputFile, "_____________\n");
+    
+}
+
+double ** reverseMatrix(double** matrix, int m, int n)
+{
+    // Выделение матрицы
+    double** reverseMatrix = (int**)malloc(sizeof(int*) * n);
+    if (reverseMatrix == NULL)
+    {
+        printf("Cannot allocate memory for matrix\n");
+        exit(1);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverseMatrix[i] = (int*)malloc(sizeof(double) * m);
+        if (reverseMatrix[i] == NULL)
+        {
+            printf("Cannot allocate memory for row of matrix\n");
+            exit(1);
+        }
+    }
+    // Заполнение единичной матрицы
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            matrix[i][j] = 0;
+            if (i == j)
+            {
+                reverseMatrix[i][j] = 1;
+            }
+            else
+            {
+                reverseMatrix[i][j] = 0;
+            }
         }
     }
-
-    char* s;
-    s = (char*)malloc(sizeof(char) * m * 2 + 100);
-    char* subs;
-    for(int i = 0; i < n; i++)
+    // Преобразование к единичной
+    for (int c = 0; c < n; c++)
     {
-        int res = fgets(s, n*2-1, inputFile);
-
-        subs = strtok(s, " ");
-        for(int j = 0; j < m; j++)
+        double coef = matrix[c][c];
+        for(int i = 0; i < m; i++)
         {
-            *(*(matrix + i)+j) = atoi(subs);
+            matrix[c][i] = matrix[c][i] / coef;
+            reverseMatrix[c][i] = reverseMatrix[c][i] / coef;
         }
+        for (int i = 0; i < m; i++)
+        {
+            if (i == c) continue;
+            coef = matrix[i][c];
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] -= matrix[c][j] * coef;
+                reverseMatrix[i][j] -= reverseMatrix[c][j] * coef;
+            }
+        }
+       
     }
-    free(s);
-    free(subs);
+    free(matrix);
+    matrix = reverseMatrix;
     return matrix;
 }
 
-void cleanMatrix(int **matrix, int m, int n)
-{
 
-}
 
-void printMatrix(int **matrix, int m, int n)
-{
-    FILE *outputFile;
-    fopen("out.txt","w");
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            printf("%d ");
-        }
-        printf("\n");
-    }
-}
-
-void reverseMatrix(int** matrix, int m, int n)
-{
-
-}
 
 int main()
 {
-    FILE* inputFile = fopen("in.txt", "r");
+    FILE* outputFile = fopen("out.txt", "w");
+    //FILE* inputFile = fopen("in.txt", "r");
     int m, n;
-    fscanf(inputFile, "%d %d\n", &m, &n);
-    int **matrix = loadMatrix(inputFile, m, n);
-    printMatrix(matrix, m, n);
-    
+    m = 5;
+    n = 5;
+    //fscanf(inputFile, "%d %d\n", &m, &n);
+    double **matrix = loadMatrix(/*inputFile,*/ m, n);
+    printMatrix(outputFile, matrix, m, n);
+    matrix = reverseMatrix(matrix, m, n);
+    printMatrix(outputFile, matrix, m, n);
+    free(matrix);
+    fclose(outputFile);
     return 0;
 }
